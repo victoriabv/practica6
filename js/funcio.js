e56146ee335553e497fe639622d9f29a40689b7a
victoria <victoriabinimelis@gmail.com>
2017-12-07 13:52:38 +0100

    commit

--- js/funcio.js
+++ js/funcio.js
@@ -1,10 +1,10 @@
-function numprimers (numero) {
+function numprimers (numero) {//funció per comprovar si un número és primer o no//
     var i=2;
     while(i<numero) {
         if(numero%i==0) {
             return false;//és un número primer//
         }
         i++;
-        return true;//no és un número primer//
     }
+    return true;//no és un número primer//
 }
